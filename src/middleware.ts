3349abc3ee70aaf3f9316017f6619995a39d724d
Jeff McKean <jmckean44@gmail.com>
2026-03-01 12:40:12 -0500

    lighthouse

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { defineMiddleware } from 'astro/middleware';
+
+const SECURITY_CSP = [
+	"default-src 'self'",
+	"base-uri 'self'",
+	"object-src 'none'",
+	"frame-ancestors 'none'",
+	"script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com https://www.googletagmanager.com https://www.google-analytics.com https://unpkg.com",
+	"connect-src 'self' https://challenges.cloudflare.com https://www.google-analytics.com https://region1.google-analytics.com",
+	"img-src 'self' data: https:",
+	"style-src 'self' 'unsafe-inline'",
+	"font-src 'self' data:",
+	'frame-src https://challenges.cloudflare.com',
+	"form-action 'self'",
+	'trusted-types default',
+	"require-trusted-types-for 'script'",
+	'upgrade-insecure-requests',
+].join('; ');
+
+export const onRequest = defineMiddleware(async (_context, next) => {
+	const response = await next();
+
+	response.headers.set('Content-Security-Policy', SECURITY_CSP);
+	response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+	response.headers.set('X-Frame-Options', 'DENY');
+	response.headers.set('X-Content-Type-Options', 'nosniff');
+	response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+	response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+
+	return response;
+});
